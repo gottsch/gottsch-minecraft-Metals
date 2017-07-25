6d59f8be8d1a8805c246ae0e8d0bf82e4f652b50
gottsch <mark.gottschling@gmail.com>
2017-07-24 20:39:03 -0400

    Added missing recipes.

--- Metals1.12/src/com/someguyssoftware/metals/Metals.java
+++ Metals1.12/src/com/someguyssoftware/metals/Metals.java
@@ -53,7 +53,7 @@ public class Metals extends AbstractMod {
 	// constants
 	public static final String MODID = "sgs_metals";
 	protected static final String NAME = "Metals";
-	protected static final String VERSION = "3.0.0";
+	protected static final String VERSION = "3.0.1";
 	protected static final String UPDATE_JSON_URL = "https://raw.githubusercontent.com/gottsch/gottsch-minecraft-Metals/master/Metals1.12/update.json";
 
 	private static final String VERSION_URL = "https://www.dropbox.com/s/at0n9db2a3sifl4/metals-versions.json?dl=1";
